ef0b8b1248f34f68eb4ebe8fdee80db5a4df4ea3
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-13 14:31:38 -0300

    Criar classe para testes de lista de estoque

---
+++ src/test/java/br/com/zup/ListaEstoque.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+import org.junit.Before;
+
+public class ListaEstoque {
+
+    private LIstaEstoque lIstaEstoque;
+
+    @Before
+    public void setup() {
+        lIstaEstoque = new LIstaEstoque();
+    }
+}
